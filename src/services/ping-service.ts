074f6f6d5ee9feabcf40ecfa62661d2e7e4f5284
John Fedoruk <johnny@johnfedoruk.ca>
2018-08-03 19:01:31 -0500

    Created a simple PingService class

---
+++ src/services/ping-service.ts
@@ -0,0 +1,9 @@
+export class PingService {
+    private response: string;
+    constructor() {
+        this.response = 'PONG';
+    }
+    public async ping(): Promise<string> {
+        return this.response;
+    }
+}
