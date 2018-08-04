aa00c06a93a825b80bf461ab6a79d98591e3a12a
John Fedoruk <johnny@johnfedoruk.ca>
2018-08-03 19:02:04 -0500

    Added a PingController, extending HttpController, that responds with PingService.ping() return value.

---
+++ src/controllers/ping.controller.ts
@@ -0,0 +1,17 @@
+import { HttpController } from '@yellow-snow/http';
+import { Resolve } from 'tsnode-di';
+
+import { PingService } from '../services/ping-service';
+
+export class PingController extends HttpController {
+    @Resolve(PingService)
+    private ping_service!: PingService;
+    public async ping(): Promise<void> {
+        try {
+            const response: string = await this.ping_service.ping();
+            this.res.send(response);
+        } catch (e) {
+            console.error(e);
+        }
+    }
+}
