ae1d72e0403f8e5ce60fe512784d4bfad798fc44
John Fedoruk <johnny@johnfedoruk.ca>
2018-08-05 22:22:58 -0500

    Added @AccessLog to PingController

--- src/controllers/ping.controller.ts
+++ src/controllers/ping.controller.ts
@@ -2,16 +2,19 @@ import { HttpController } from "@yellow-snow/http";
 import { Resolve } from "tsnode-di";
 
 import { PingService } from "../services/ping-service";
+import { AccessLog } from "../decorators/access-log";
 
 export class PingController extends HttpController {
     @Resolve(PingService)
     private ping_service!: PingService;
+    @AccessLog()
     public async ping(): Promise<void> {
         try {
             const response: string = await this.ping_service.ping();
             this.res.send(response);
         } catch (e) {
             console.error(e);
+            this.res.sendStatus(500);
         }
     }
 }
