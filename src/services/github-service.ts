6c9880b75a076406decc5e8feabd127aba05b1e0
John Fedoruk <johnny@johnfedoruk.ca>
2018-09-02 00:50:28 -0500

    Added method getGraph to GithubService

--- src/services/github-service.ts
+++ src/services/github-service.ts
@@ -1,9 +1,11 @@
 import Axios, { AxiosStatic, AxiosResponse } from 'axios';
 import { config } from '../config';
 import * as LRU from 'lru-cache';
+import * as cheerio from 'cheerio';
 
 export class GithubService {
     private contributions: LRU.Cache<string, number> = new LRU(config.github.lru);
+    private graphs: LRU.Cache<string, string> = new LRU(config.github.lru);
     private axios: AxiosStatic = Axios;
     public async getContributions(username: string): Promise<number> {
         let contributions: number | undefined = this.contributions.get(username);
@@ -17,6 +19,20 @@ export class GithubService {
             contributions = +(<string>(<any>body).match(regex)[1].replace(/[^0-9]/g, ''));
             this.contributions.set(username, contributions);
             return contributions;
+        }
+    }
+    public async getGraph(username: string): Promise<string> {
+        let graph: string | undefined = this.graphs.get(username);
+        if (graph !== undefined) {
+            return graph;
+        } else {
+            const url: string = `https://github.com/users/${username}/contributions`;
+            const res: AxiosResponse<string> = await this.axios.get(url);
+            const body: string = <string>res.data;
+            const $ = cheerio.load(body);
+            graph = <string>$('.js-calendar-graph-svg').parent().html();
+            this.graphs.set(username, graph);
+            return graph;
         }
     }
 }
