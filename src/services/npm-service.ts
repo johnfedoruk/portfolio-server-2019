1b8d6be7429e43e2402c38e9b2bbf0e50a161383
John Fedoruk <johnny@johnfedoruk.ca>
2018-09-02 13:55:02 -0500

    Added caching to NpmService

--- src/services/npm-service.ts
+++ src/services/npm-service.ts
@@ -1,15 +1,23 @@
 import * as NpmApi from 'npm-api';
+import * as LRU from 'lru-cache';
+import { config } from '../config';
 
 export class NpmService {
+    private packages: LRU.Cache<string, any[]> = new LRU(config.npm.lru);
     private npm = new NpmApi();
     public async listPackages(username: string): Promise<any> {
-        const maintainer = await this.npm.maintainer(username);
-        const repos = await maintainer.repos();
-        const pkgs = [];
-        for (let repo of repos) {
-            const pkg = await this.npm.repo(repo);
-            pkgs.push(await pkg.package());
+        let pkgs: any[] | undefined = this.packages.get(username);
+        if ( pkgs !== undefined ) {
+            return pkgs;
+        } else {
+            pkgs = [];
+            const maintainer = await this.npm.maintainer(username);
+            const repos = await maintainer.repos();
+            for (let repo of repos) {
+                const pkg = await this.npm.repo(repo);
+                pkgs.push(await pkg.package());
+            }
+            return pkgs;
         }
-        return pkgs;
     }
 }
