1c3549c3e070d9aebea855511b75e55034263525
John Fedoruk <johnny@johnfedoruk.ca>
2018-09-02 18:40:33 -0500

    Added a getReadme method to GithubService

--- src/services/github-service.ts
+++ src/services/github-service.ts
@@ -7,6 +7,7 @@ import { Log } from '../decorators/log';
 export class GithubService {
     private contributions: LRU.Cache<string, number> = new LRU(config.github.lru);
     private graphs: LRU.Cache<string, string> = new LRU(config.github.lru);
+    private readmes: LRU.Cache<[string, string], string> = new LRU(config.github.lru);
     private axios: AxiosStatic = Axios;
     @Log()
     public async getContributions(username: string): Promise<number> {
@@ -36,6 +37,22 @@ export class GithubService {
             graph = <string>$('.js-calendar-graph-svg').parent().html();
             this.graphs.set(username, graph);
             return graph;
+        }
+    }
+    @Log()
+    public async getReadme(username: string, repository: string): Promise<string> {
+        // const key: string = `${username}/${repository}`;
+        let readme: string | undefined = this.readmes.get([username, repository]);
+        if (readme !== undefined) {
+            console.log('found');
+            return readme;
+        } else {
+            const url: string = `https://raw.githubusercontent.com/${username}/${repository}/master/README.md`;
+            const res: AxiosResponse<string> = await this.axios.get(url);
+            const readme: string = <string>res.data;
+            console.log('saving');
+            this.readmes.set([username, repository], readme);
+            return readme;
         }
     }
 }
